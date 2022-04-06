ed8a8d96ddd70506bc89b50ad25cc6eca43f6020
Nadav Inbar <nadavinbar8@gmail.com>
2022-04-06 11:38:39 +0300

    .

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -2,8 +2,9 @@ import Piano from '../components/Piano.jsx';
 export const Homepage = () => {
   return (
     <div className='homepage'>
-      <h1>Hello World!</h1>
+      <h1>React Piano!</h1>
       <Piano />
+      <h3>Use the z, x, c, v, b, n, m keys to play!</h3>
     </div>
   );
 };
